de9eb18eeec74330a1b044cd05b3fb5467689767
CesarJico <j.c.martinez.g@outlook.es>
2023-06-21 22:34:33 -0600

    Updates

--- src/utilis/FetchPhot.js
+++
@@ -1,6 +0,0 @@
-export const fetchPhotosData = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/photos");
-    const dataPhotos = await response.json();
-    // console.debug(dataPhotos);
-    return dataPhotos;
-}
